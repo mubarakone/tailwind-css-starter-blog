ee5a7d3c041a9f48427993ca9e7380ea50f42130
mubarakone <52806204+mubarakone@users.noreply.github.com>
2024-03-20 14:09:20 -0700

    fix build 5.0

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@
 //   workboxOptions: "production",
 // });
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 const { GenerateSW } = require('workbox-webpack-plugin');
 
 const { withContentlayer } = require('next-contentlayer')
