af21931f010e323022c0f6a1a4e1a7de195fdfc5
mubarakone <52806204+mubarakone@users.noreply.github.com>
2024-04-08 16:47:08 -0700

    fix frame 52

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   workboxOptions: {
     mode: "production",
-    GenerateSW: GenerateSW,
+    GenerateSW: "exclude",
   },
 });
 
